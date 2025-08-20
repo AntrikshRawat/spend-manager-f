90dccc35f860f05f5b1017f14316aab28821b522
Antriksh rawat <antriksh.rawat@aitner.ai>
2025-08-20 14:54:57 +0530

    update

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener("push", (event) => {
   const data = event.data.json();
 
   self.registration.showNotification(data.title, {
-    body: data.message,
+    body: data.body,
     icon: "/logo-256x256.png",
     badge: "/logo-192x192.png" 
   });
