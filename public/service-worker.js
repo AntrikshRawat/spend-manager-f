c646ea8c8400d64dfa5e1d34efd82d823dc0ab1b
Antriksh rawat <antriksh.rawat@aitner.ai>
2025-08-21 18:07:07 +0530

    update

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,6 +3,26 @@ self.addEventListener("push", (event) => {
 
   self.registration.showNotification(data.title, {
     body: data.body,
-    icon: "/logo-256x256.png", 
+    icon: "/logo-256x256.png",
+    data: { url: data.url },
   });
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close(); // close the notification
+
+  const url = `https://spend-manager-f.vercel.app${event.notification.data.url}`; // get URL from data
+
+  event.waitUntil(
+    self.clients.matchAll({ type: "window", includeUncontrolled: true }).then((clientList) => {
+      for (const client of clientList) {
+        if (client.url === url && "focus" in client) {
+          return client.focus();
+        }
+      }
+      if (self.clients.openWindow) {
+        return self.clients.openWindow(url);
+      }
+    })
+  );
 });
