b5331b9cf83fcdbd71e4a1db5e5f02e4b623ded4
Antriksh rawat <antrikshrawat2@gmail.com>
2026-08-17 00:25:05 +0530

    added page transition

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
+import PageTransition from './PageTransition'
 
 export default function RootLayout() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-indigo-100 via-purple-50 to-pink-100">
       <Header />
       <main className="pt-16">
-        <Outlet />
+        <PageTransition>
+          <Outlet />
+        </PageTransition>
       </main>
     </div>
   )
